80876fa2a5bcb798e563c44eef69bbf2223adc72
moritz91 <moritz.giller@hotmail.com>
2018-12-26 00:24:33 +0100

    frontend types with codegen

--- packages/web/pages/register.tsx
+++ packages/web/pages/register.tsx
@@ -3,6 +3,8 @@ import { Formik, Field } from "formik";
 import { InputField } from "../components/formik-fields/InputField";
 import { registerSchema } from "@stackflow/common";
 import { ErrorMessage } from "../components/ErrorMessage";
+import { normalizeErrors } from "../utils/normalizeErrors";
+import { RegisterMutationComponent } from "../components/apollo-components";
 
 interface FormValues {
   username: string;
@@ -11,36 +13,49 @@ interface FormValues {
 }
 
 export default () => (
-  <Formik<FormValues>
-    initialValues={{ username: "", email: "", password: "" }}
-    onSubmit={() => {}}
-    validationSchema={registerSchema}
-    validateOnBlur={false}
-    validateOnChange={false}
-  >
-    {({ errors, handleSubmit }) => {
-      <Form onSubmit={handleSubmit}>
-        <Field
-          name="username"
-          label="Username"
-          placeholder="Username"
-          component={InputField}
-        />
-        <Field
-          name="email"
-          label="Email"
-          placeholder="Email"
-          component={InputField}
-        />
-        <Field
-          name="password"
-          label="Password"
-          placeholder="Password"
-          component={InputField}
-        />
-        <ErrorMessage errors={errors} />
-        <Button type="submit">Create Account</Button>
-      </Form>;
-    }}
-  </Formik>
+  <RegisterMutationComponent>
+    {mutate => (
+      <Formik<FormValues>
+        initialValues={{ username: "", email: "", password: "" }}
+        onSubmit={async (input, { setErrors, setSubmitting }) => {
+          const response = await mutate({
+            variables: { input }
+          });
+          if (response && response.data && response.data.register.errors) {
+            return setErrors(normalizeErrors(response.data.register.errors));
+          } else {
+            // navigate screen
+          }
+        }}
+        validationSchema={registerSchema}
+        validateOnBlur={false}
+        validateOnChange={false}
+      >
+        {({ errors, handleSubmit }) => {
+          <Form onSubmit={handleSubmit}>
+            <Field
+              name="username"
+              label="Username"
+              placeholder="Username"
+              component={InputField}
+            />
+            <Field
+              name="email"
+              label="Email"
+              placeholder="Email"
+              component={InputField}
+            />
+            <Field
+              name="password"
+              label="Password"
+              placeholder="Password"
+              component={InputField}
+            />
+            <ErrorMessage errors={errors} />
+            <Button type="submit">Create Account</Button>
+          </Form>;
+        }}
+      </Formik>
+    )}
+  </RegisterMutationComponent>
 );
